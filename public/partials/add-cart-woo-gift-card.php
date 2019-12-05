fe328ab91b58971e30cf623ba9f73d960793152b
Richard Muvirimi <tygalive@gmail.com>
2019-12-05 23:59:55 +0200

    added templating field on front end

--- public/partials/add-cart-woo-gift-card.php
+++ public/partials/add-cart-woo-gift-card.php
@@ -30,14 +30,14 @@ if ($pricing !== 'fixed'):
 
 		$range = wc_format_price_range(wc_get_price_to_display($product, array('price' => $min)) . $product->get_price_suffix($min), wc_get_price_to_display($product, array('price' => $max)) . $product->get_price_suffix($max));
 		?>
-	        <input type="number" min="<?php echo esc_attr($min) ?>" max="<?php echo esc_attr($max) ?>" class="woocommerce-Input woocommerce-Input--text input-text" name="wgc-receiver-price" id="wgc-receiver-price" value="<?php echo esc_attr($price); ?>" required/>
+	        <input type="number" min="<?php esc_attr_e($min) ?>" max="<?php esc_attr_e($max) ?>" class="woocommerce-Input woocommerce-Input--text input-text" name="wgc-receiver-price" id="wgc-receiver-price" value="<?php esc_attr_e($price); ?>" required/>
 	        <span><em><?php echo sprintf(esc_html('Enter a price value in the range %s', 'woo-gift-card'), $range); ?></em></span>
 		<?php
 		break;
 	    case 'user':
 		$default = $product->get_meta('wgc-price-user');
 		?>
-	        <input type="number" min="0" class="woocommerce-Input woocommerce-Input--text input-text" name="wgc-receiver-price" id="wgc-receiver-price" value="<?php echo esc_attr($default); ?>" required/>
+	        <input type="number" min="0" class="woocommerce-Input woocommerce-Input--text input-text" name="wgc-receiver-price" id="wgc-receiver-price" value="<?php esc_attr_e($default); ?>" required/>
 	        <span><em><?php esc_html_e('Enter a price value for the gift voucher', 'woo-gift-card') ?></em></span>
 		<?php
 		break;
@@ -45,7 +45,7 @@ if ($pricing !== 'fixed'):
 		$prices = explode("|", $product->get_meta('wgc-price-selected'));
 		foreach ($prices as $price) :
 		    ?>
-		    <input type="radio" class="woocommerce-Input woocommerce-Input--text input-radio" name="wgc-receiver-price" id="wgc-receiver-price" value="<?php echo esc_attr($price); ?>" required <?php checked($prices[0], $price); ?>/>
+		    <input type="radio" class="woocommerce-Input woocommerce-Input--text input-radio" name="wgc-receiver-price" id="wgc-receiver-price" value="<?php esc_attr_e($price); ?>" required <?php checked($prices[0], $price); ?>/>
 		    <span><?php echo wc_price(wc_get_price_to_display($product, array('price' => $price))) . $product->get_price_suffix($price) ?></span>
 		    <?php
 		endforeach;
@@ -65,22 +65,33 @@ if ($product->is_virtual()):
 
         <p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
     	<label for="wgc-receiver-name"><?php esc_html_e('Receiver name', 'woo-gift-card'); ?>&nbsp;<span class="required">*</span></label>
-    	<input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="wgc-receiver-name" id="wgc-receiver-name" value="<?php echo esc_attr(get_user_option("display_name")); ?>" required/>
+    	<input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="wgc-receiver-name" id="wgc-receiver-name" value="<?php esc_attr_e(get_user_option("display_name")); ?>" required/>
         </p>
 
         <p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
     	<label for="wgc-receiver-email"><?php esc_html_e('Receiver email', 'woo-gift-card'); ?>&nbsp;<span class="required">*</span></label>
-    	<input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="wgc-receiver-email" id="wgc-receiver-email" value="<?php echo esc_attr(get_user_option("user_email")); ?>" required/>
+    	<input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="wgc-receiver-email" id="wgc-receiver-email" value="<?php esc_attr_e(get_user_option("user_email")); ?>" required/>
         </p>
 
         <p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
     	<label for="wgc-receiver-message"><?php esc_html_e('Receiver message', 'woo-gift-card'); ?></label>
-    	<textarea class="woocommerce-Input woocommerce-Input--text input-text" name="wgc-receiver-message" id="wgc-receiver-message" rows="3" maxlength="<?php echo esc_attr(get_option('wgc-message-length')) ?>"></textarea>
+    	<textarea class="woocommerce-Input woocommerce-Input--text input-text" name="wgc-receiver-message" id="wgc-receiver-message" rows="3" maxlength="<?php esc_attr_e(get_option('wgc-message-length')) ?>"></textarea>
     	<span id="wgc-message-length"></span>
         </p>
 
         <!-- if has template file-->
-	<?php if (!empty($product->get_meta('wgc-template'))): ?>
+	<?php
+	$templates = $product->get_meta('wgc-template');
+	if (!empty($templates)):
+	    ?>
+	    <p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
+		<label for="wgc-receiver-template"><?php esc_html_e('Gift Voucher template', 'woo-gift-card'); ?></label>
+		<?php foreach ($templates as $template) : ?>
+	    	<input type="radio" class="woocommerce-Input woocommerce-Input--text input-radio" name="wgc-receiver-template" id="wgc-receiver-template" value="<?php esc_attr_e($template); ?>" required <?php checked($templates[0], $template); ?>/>
+	    	<span><?php esc_html_e(get_post_field('post_title', $template)) ?></span>
+		<?php endforeach; ?>
+	    </p>
+
 	    <p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
 		<label for="wgc-receiver-image"><?php esc_html_e('Gift Voucher image', 'woo-gift-card'); ?></label>
 		<input type="file" class="woocommerce-Input woocommerce-Input--text input-text" name="wgc-receiver-image" id="wgc-receiver-image"/>
@@ -96,7 +107,7 @@ if ($product->is_virtual()):
     <?php if (!empty($product->get_meta('wgc-schedule'))): ?>
 	<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
 	    <label for="wgc-receiver-schedule"><?php esc_html_e('Date to send Gift Voucher', 'woo-gift-card'); ?></label>
-	    <input type="date" class="woocommerce-Input woocommerce-Input--text input-text" name="wgc-receiver-schedule" id="wgc-receiver-schedule" value="<?php echo esc_attr(date('Y-m-d')) ?>"  min="<?php echo esc_attr(date('Y-m-d')) ?>"/>
+	    <input type="date" class="woocommerce-Input woocommerce-Input--text input-text" name="wgc-receiver-schedule" id="wgc-receiver-schedule" value="<?php esc_attr_e(date('Y-m-d')) ?>"  min="<?php esc_attr_e(date('Y-m-d')) ?>"/>
 	</p>
     <?php endif; ?>
 
@@ -125,23 +136,23 @@ endif;
 
   <!--<p class="woocommerce-form-row woocommerce-form-row--first form-row form-row-first">
     <label for="account_first_name"><?php esc_html_e('First name', 'woocommerce'); ?>&nbsp;<span class="required">*</span></label>
-    <input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="account_first_name" id="account_first_name" autocomplete="given-name" value="<?php echo esc_attr($user->first_name); ?>" />
+    <input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="account_first_name" id="account_first_name" autocomplete="given-name" value="<?php esc_attr_e($user->first_name); ?>" />
 </p>
 <p class="woocommerce-form-row woocommerce-form-row--last form-row form-row-last">
     <label for="account_last_name"><?php esc_html_e('Last name', 'woocommerce'); ?>&nbsp;<span class="required">*</span></label>
-    <input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="account_last_name" id="account_last_name" autocomplete="family-name" value="<?php echo esc_attr($user->last_name); ?>" />
+    <input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="account_last_name" id="account_last_name" autocomplete="family-name" value="<?php esc_attr_e($user->last_name); ?>" />
 </p>
 <div class="clear"></div>
 
 <p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
     <label for="account_display_name"><?php esc_html_e('Display name', 'woocommerce'); ?>&nbsp;<span class="required">*</span></label>
-    <input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="account_display_name" id="account_display_name" value="<?php echo esc_attr($user->display_name); ?>" /> <span><em><?php esc_html_e('This will be how your name will be displayed in the account section and in reviews', 'woocommerce'); ?></em></span>
+    <input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="account_display_name" id="account_display_name" value="<?php esc_attr_e($user->display_name); ?>" /> <span><em><?php esc_html_e('This will be how your name will be displayed in the account section and in reviews', 'woocommerce'); ?></em></span>
 </p>
 <div class="clear"></div>
 
 <p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
     <label for="account_email"><?php esc_html_e('Email address', 'woocommerce'); ?>&nbsp;<span class="required">*</span></label>
-    <input type="email" class="woocommerce-Input woocommerce-Input--email input-text" name="account_email" id="account_email" autocomplete="email" value="<?php echo esc_attr($user->user_email); ?>" />
+    <input type="email" class="woocommerce-Input woocommerce-Input--email input-text" name="account_email" id="account_email" autocomplete="email" value="<?php esc_attr_e($user->user_email); ?>" />
 </p>-->
 
 
